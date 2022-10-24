6e5c14c285eb244e5ae127cc0778caa3235bd42d
woong7361 <woong7361@gmail.com>
2022-10-24 17:03:19 +0900

    [init] member entity 초안

---
+++ src/main/java/june/songmelier/entity/Member.java
@@ -0,0 +1,36 @@
+package june.songmelier.entity;
+
+import june.songmelier.entity.superclass.TimeStamped;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends TimeStamped {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String username;
+    private String imageUrl;
+    private String introduce;
+
+
+//--------------------------------------------------- 생성자 ----------------------------------------------------------//
+    public Member(String email, String password, String username, String imageUrl, String introduce) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.imageUrl = imageUrl;
+        this.introduce = introduce;
+    }
+}
