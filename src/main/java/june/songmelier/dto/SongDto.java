2be820585fa10f265dcb82c2d484694bbbbf514e
gahee99 <u1879@sookmyung.ac.kr>
2022-12-15 14:35:48 +0900

    [feature] 곡 상세 정보 확인 api 개발 #1

---
+++ src/main/java/june/songmelier/dto/SongDto.java
@@ -0,0 +1,51 @@
+package june.songmelier.dto;
+
+
+import june.songmelier.entity.Bookmark;
+import june.songmelier.entity.Favor;
+import june.songmelier.entity.Song;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.awt.print.Book;
+
+public class SongDto {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static public class SongRes {
+        private Long songId;
+        private String title;
+        private String singer;
+        private String imageUrl;
+        private String publishDate;
+        private Long favorCount;
+        private Long scrapCount;
+        private Long commentCount;
+        private String rapDifficult;
+        private String highDifficult;
+        private String mood;
+        private boolean isMySinglist;
+        private boolean isFavor;
+
+
+        public SongRes(Song song, boolean isFavor, boolean isMySinglist) {
+            this.songId = song.getId();
+            this.title = song.getTitle();
+            this.singer = song.getSinger();
+            this.imageUrl = song.getImageUrl();
+            this.publishDate = song.getPublishedDate();
+            this.favorCount = song.getFavorCount();
+            this.scrapCount = song.getBookmarkCount();
+            this.commentCount = song.getCommentCount();
+            this.rapDifficult = song.getRapDifficult();
+            this.highDifficult = song.getHighDifficult();
+            this.mood = song.getMood();
+            this.isMySinglist = isMySinglist;
+            this.isFavor = isFavor;
+
+        }
+    }
+}
